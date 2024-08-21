418dd835037bfd509bf8b67dd29fbea854abfbe2
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-08-21 17:35:42 +0100

    Favicon and title set as intended

--- booking-manager/src/index.js
+++ booking-manager/src/index.js
@@ -5,6 +5,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const favicon = document.createElement('link');
+favicon.rel = 'shortcut icon';
+favicon.href = '/VERSION 1.png';
+document.head.appendChild(favicon);
+document.title = 'Favour Express';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>

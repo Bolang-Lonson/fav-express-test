661fdced841b93e518a08a3abe4d48448bfbc21e
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-08-27 17:39:22 +0100

    ...

--- booking-manager/src/pages/landing/track.jsx
+++ booking-manager/src/pages/landing/track.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const Track = () => {
-    const {id} = useParams()
+    const {number} = useParams()
   return (
-    <div>Track {id}</div>
+    <div>Track {number}</div>
   );
 }
 

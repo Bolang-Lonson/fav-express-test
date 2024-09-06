0d517782008693871709c254386b6dc5a86d631f
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-09-06 15:49:50 +0100

    delete functionality for tickets implemented

--- booking-manager/src/pages/layout/views/tickets.jsx
+++ booking-manager/src/pages/layout/views/tickets.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import logo from '../../../assets/VERSION 2.png';
 import QR from '../../../assets/QR_Code-removebg-preview 1.png'
@@ -8,19 +8,36 @@ import {useMediaQuery} from 'react-responsive';
 
 const Tickets = ({tickets}) => {
 
+	const [ticks, setTicks] = useState(tickets);
+	useEffect(() => {
+		const storedTickets = localStorage.getItem("tickets");
+		if (storedTickets) {
+			setTicks(JSON.parse(storedTickets));
+		}
+	}, []);
 	const navigate = useNavigate();
+
+	const deleteAll = (event) => {
+		event.preventDefault();
+		localStorage.removeItem("tickets");
+		window.location.reload();
+	}
 
   return (
     <div className='pb-32 pt-[5.5rem] md:pt-28'>
 		{
-		tickets.length === 0?
+		ticks.length === 0?
 		<div className='flex flex-col items-center h-[75vh] justify-center gap-6'>
 			<p className="text-2xl font-roboto">No tickets available now</p>
 			<a href="/home" className='underline text-favblue font-roboto'>Book Now</a>
 		</div>
 		:
 		<div className='flex flex-col w-[90%] md:w-4/5 lg:w-3/5 mx-auto gap-5'>
-			{tickets.map((ticket, idx) => (
+			<button 
+				className="bg-red bg-opacity-90 text-white max-w-fit px-5 py-[0.375rem] rounded-lg ms-auto active:bg-opacity-100"
+				onClick={deleteAll}
+			>Delete All <i className="bi bi-trash-fill"></i></button>
+			{ticks.map((ticket, idx) => (
 				<div 
 					className="ticket bg-white w-full rounded-xl flex flex-col cursor-pointer" key={idx}
 					onClick={() => navigate(`/tickets/${idx + 1}`)}
@@ -37,8 +54,16 @@ const Tickets = ({tickets}) => {
 							<span className='bi bi-circle-fill text-gray-200 text-xs'></span>
 							<span className='text-favblue font-semibold font-roboto'>{ticket.to.name}</span>
 						</div>
-						<div className="basis-2/5 flex items-center justify-end my-2">
+						<div className="basis-2/5 flex flex-col items-end justify-end my-2 gap-8">
 							<span className='bg-favblue rounded-lg text-white px-4 py-2 font-roboto text-md'>XAF {ticket['amount']}</span>
+							<button 
+								className="basis-full text-red py-2 px-5"
+								onClick={() => {
+									const ticksArr = JSON.parse(localStorage.tickets);
+									localStorage.setItem("tickets", JSON.stringify(ticksArr.splice(idx, 1)));
+									window.location.reload();
+								}}
+							>Delete <i className="bi bi-trash"></i></button>
 						</div>
 					</div>
 					<div className="border-t border-dashed border-black py-3 px-5 md:py-6 md:px-24 flex items-center justify-between">
